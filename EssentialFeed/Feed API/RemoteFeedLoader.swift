4c3beb7a0663d4be3577b066e55fe5799a2cff9c
Lennart Wisbar <lewisbar.git@gmail.com>
2022-12-09 16:26:28 +0100

    move mapping logic to new type `FeedItemsMapper

--- EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -39,12 +39,10 @@ public final class RemoteFeedLoader {
         client.get(from: url) { result in
             switch result {
             case let .success(data, response):
-                if response.statusCode == 200,
-                   let root = try? JSONDecoder().decode(Root.self, from: data) {
-
-                    // Mapping from network to domain
-                    completion(.success(root.items.map { $0.item }))
-                } else {
+                do {
+                    let items = try FeedItemsMapper.map(data, response)
+                    completion(.success(items))
+                } catch {
                     completion(.failure(.invalidData))
                 }
             case .failure:
@@ -54,19 +52,33 @@ public final class RemoteFeedLoader {
     }
 }
 
-private struct Root: Decodable {
-    let items: [Item]
-}
+private class FeedItemsMapper {
+    private struct Root: Decodable {
+        let items: [Item]
+    }
 
-// Instead of decoding directly to FeedItem, which would couple FeedItem with this specific API, where it's "image" instead of "imageURL", for example, so you would need coding keys, but different APIs could have different names. So if one changes, they all break. So instead, we have an API specific private Decodable struct Item here that we can conveniently decode the data from the server to, and then map this data to the domain model FeedItem.
-// The RemoteFeedLoader knows the FeedItem, as the diagram in the lecture also shows. But the FeedItem and the HTTPClient(s) all know no one else. So the RemoteFeedLoader seems to be an Adapter between the network/data layer and the domain layer.
-private struct Item: Decodable {
-    let id: UUID
-    let description: String?
-    let location: String?
-    let image: URL
+    /// API specific struct to decode from data and map to FeedItem
+    /// If we would instead decode to FeedItem directly, we would couple FeedItem to the specific API.
+    private struct Item: Decodable {
+        let id: UUID
+        let description: String?
+        let location: String?
+        let image: URL
 
-    var item: FeedItem {
-        FeedItem(id: id, description: description, location: location, imageURL: image)
+        var item: FeedItem {
+            FeedItem(id: id, description: description, location: location, imageURL: image)
+        }
+    }
+
+    // Mapping from network to domain
+    static func map(_ data: Data, _ response: HTTPURLResponse) throws -> [FeedItem] {
+        guard response.statusCode == 200 else {
+            throw RemoteFeedLoader.Error.invalidData
+        }
+
+        return try JSONDecoder()
+            .decode(Root.self, from: data)
+            .items
+            .map { $0.item }
     }
 }
