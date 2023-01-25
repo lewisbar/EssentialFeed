f1caa23b397b2a007ae9b7587bbce22e6839438d
Lennart Wisbar <lewisbar.git@gmail.com>
2023-01-25 23:37:51 +0100

    retrieve delivers failure on retrieval error (invalid cached data)

--- EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -47,9 +47,14 @@ class CodableFeedStore {
             return completion(.empty)
         }
 
-        let decoder = JSONDecoder()
-        let cache = try! decoder.decode(Cache.self, from: data)
-        completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+        do {
+            let decoder = JSONDecoder()
+            let cache = try decoder.decode(Cache.self, from: data)
+            completion(.found(feed: cache.localFeed, timestamp: cache.timestamp))
+        } catch {
+            completion(.failure(error))
+        }
+
     }
 
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
@@ -104,6 +109,14 @@ final class CodableFeedStoreTests: XCTestCase {
         expect(sut, toRetrieveTwice: .found(feed: feed, timestamp: timestamp))
     }
 
+    func test_retrieve_deliversFailureOnRetrievalError() {
+        let sut = makeSUT()
+
+        try! "invalid data".write(to: testSpecificStoreURL(), atomically: false, encoding: .utf8)
+
+        expect(sut, toRetrieve: .failure(anyNSError()))
+    }
+
     private func makeSUT(file: StaticString = #filePath, line: UInt = #line) -> CodableFeedStore {
         let storeURL = testSpecificStoreURL()
         let sut = CodableFeedStore(storeURL: storeURL)
@@ -136,6 +149,12 @@ final class CodableFeedStoreTests: XCTestCase {
                     break
                 }
 
+            case .failure:
+                guard case .failure = retrievedResult else {
+                    XCTFail("Expected .failure, got \(retrievedResult) instead", file: file, line: line)
+                    break
+                }
+
             case let .found(expectedFeed, expectedTimestamp):
                 guard case let .found(retrievedFeed, retrievedTimestamp) = retrievedResult else {
                     XCTFail("Expected \(expectedResult), got \(retrievedResult)", file: file, line: line)
@@ -143,9 +162,6 @@ final class CodableFeedStoreTests: XCTestCase {
                 }
                 XCTAssertEqual(retrievedFeed, expectedFeed, file: file, line: line)
                 XCTAssertEqual(retrievedTimestamp, expectedTimestamp, file: file, line: line)
-
-            default:
-                XCTFail("Expected to retrieve \(expectedResult), got \(retrievedResult) instead", file: file, line: line)
             }
 
             exp.fulfill()
