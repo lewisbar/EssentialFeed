683cb393678314e15425d97f77c48e5283000c0d
Lennart Wisbar <lewisbar.git@gmail.com>
2023-01-28 00:26:52 +0100

    pass feed and timestamp inline

--- EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FailableInsertFeedStoreSpecs.swift
+++ EssentialFeedTests/Feed Cache/FeedStoreSpecs/XCTestCase+FailableInsertFeedStoreSpecs.swift
@@ -10,19 +10,13 @@ import EssentialFeed
 
 extension FailableInsertFeedStoreSpecs where Self: XCTestCase {
     func assertThatInsertDeliversErrorOnInsertionError(on sut: FeedStore, file: StaticString = #filePath, line: UInt = #line) {
-        let feed = uniqueImageFeed().local
-        let timestamp = Date()
-
-        let insertionError = insert((feed, timestamp), to: sut)
+        let insertionError = insert((uniqueImageFeed().local, Date()), to: sut)
 
         XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error", file: file, line: line)
     }
 
     func assertThatInsertHasNoSideEffectsOnInsertionError(on sut: FeedStore, file: StaticString = #filePath, line: UInt = #line) {
-        let feed = uniqueImageFeed().local
-        let timestamp = Date()
-
-        insert((feed, timestamp), to: sut)
+        insert((uniqueImageFeed().local, Date()), to: sut)
 
         expect(sut, toRetrieve: .empty, file: file, line: line)
     }
