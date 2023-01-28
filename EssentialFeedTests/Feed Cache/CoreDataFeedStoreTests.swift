7a68d628fc136c8a106fb99372fefde84638bd08
Lennart Wisbar <lewisbar.git@gmail.com>
2023-01-28 22:25:16 +0100

    test that `CoreDataFeedStore` delivers empty on empty cache

---
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -0,0 +1,47 @@
+//
+//  CoreDataFeedStoreTests.swift
+//  EssentialFeedTests
+//
+//  Created by LennartWisbar on 28.01.23.
+//
+
+import XCTest
+import EssentialFeed
+
+class CoreDataFeedStore: FeedStore {
+    func retrieve(completion: @escaping RetrievalCompletion) {
+        completion(.empty)
+    }
+
+    func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {}
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {}
+}
+
+final class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        let sut = makeSUT()
+
+        assertThatRetrieveDeliversEmptyOnEmptyCache(on: sut)
+    }
+
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {}
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {}
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {}
+
+    func test_insert_deliversNoErrorOnEmptyCache() {}
+    func test_insert_deliversNoErrorOnNonEmptyCache() {}
+    func test_insert_overridesPreviouslyInsertedCacheValues() {}
+
+    func test_delete_deliversNoErrorOnEmptyCache() {}
+    func test_delete_hasNoSideEffectsOnEmptyCache() {}
+    func test_delete_deliversNoErrorOnNonEmptyCache() {}
+    func test_delete_emptiesPreviouslyInsertedCache() {}
+
+    func test_storeSideEffects_runSerially() {}
+
+
+    // MARK: - Helpers
+    private func makeSUT() -> FeedStore {
+        CoreDataFeedStore()
+    }
+}
