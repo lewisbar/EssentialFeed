e18d160a3b7c6b58e637385754325f3df92ad2d9
Lennart Wisbar <lewisbar.git@gmail.com>
2023-02-02 11:48:26 +0100

    load persistent container stores upon `CoreDataFeedStore` initialization

--- EssentialFeed/Feed Cache/CoreDataFeedStore.swift
+++ EssentialFeed/Feed Cache/CoreDataFeedStore.swift
@@ -8,7 +8,11 @@
 import CoreData
 
 public class CoreDataFeedStore: FeedStore {
-    public init() {}
+    private let container: NSPersistentContainer
+
+    public init(bundle: Bundle = .main) throws {
+        container = try NSPersistentContainer.load(modelName: "FeedStore", in: bundle)
+    }
 
     public func retrieve(completion: @escaping RetrievalCompletion) {
         completion(.empty)
@@ -16,6 +20,34 @@ public class CoreDataFeedStore: FeedStore {
 
     public func insert(_ feed: [EssentialFeed.LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {}
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {}
+}
+
+private extension NSPersistentContainer {
+    enum LoadingError: Swift.Error {
+        case modelNotFound
+        case failedToLoadPersistentStores(Swift.Error)
+    }
+
+    static func load(modelName name: String, in bundle: Bundle) throws -> NSPersistentContainer {
+        guard let model = NSManagedObjectModel.with(name: name, in: bundle) else {
+            throw LoadingError.modelNotFound
+        }
+
+        let container = NSPersistentContainer(name: name, managedObjectModel: model)
+        var loadError: Swift.Error?
+        container.loadPersistentStores { loadError = $1 }
+        try loadError.map { throw LoadingError.failedToLoadPersistentStores($0) }
+
+        return container
+    }
+}
+
+private extension NSManagedObjectModel {
+    static func with(name: String, in bundle: Bundle) -> NSManagedObjectModel? {
+        bundle
+            .url(forResource: name, withExtension: "momd")
+            .flatMap { NSManagedObjectModel(contentsOf: $0) }
+    }
 }
 
 private class ManagedCache: NSManagedObject {
