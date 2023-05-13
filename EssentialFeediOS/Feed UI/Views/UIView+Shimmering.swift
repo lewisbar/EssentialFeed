088ef427869d43b5fbbaf3184d52d7f30ca145f4
Lennart Wisbar <lewisbar.git@gmail.com>
2023-05-14 00:02:44 +0200

    Move FeedImage value translations (including image loading state) to `FeedImageViewModel`. Now, the `FeedImageCellController` acts as a binder between the `View` and the `ViewModel`

--- EssentialFeediOS/Feed UI/Views/UIView+Shimmering.swift
+++ EssentialFeediOS/Feed UI/Views/UIView+Shimmering.swift
@@ -9,12 +9,21 @@ import UIKit
 
 extension UIView {
     public var isShimmering: Bool {
-        layer.mask?.animation(forKey: shimmerAnimationKey) != nil
+        set {
+            if newValue {
+                startShimmering()
+            } else {
+                stopShimmering()
+            }
+        }
+        get {
+            layer.mask?.animation(forKey: shimmerAnimationKey) != nil
+        }
     }
     
     private var shimmerAnimationKey: String { "shimmer" }
 
-    func startShimmering() {
+    private func startShimmering() {
         let white = UIColor.white.cgColor
         let alpha = UIColor.white.withAlphaComponent(0.7).cgColor
         let width = bounds.width
@@ -35,7 +44,7 @@ extension UIView {
         gradient.add(animation, forKey: shimmerAnimationKey)
     }
 
-    func stopShimmering() {
+    private func stopShimmering() {
         layer.mask = nil
     }
 }
