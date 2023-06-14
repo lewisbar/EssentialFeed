be9e214f849c4b2253401fd33de6add25048f015
Lennart Wisbar <lewisbar.git@gmail.com>
2023-06-14 17:32:43 +0200

    Add FeedImagePresenter holding FeedImage presentation logic (including image loading state)

---
+++ EssentialFeediOS/FeedPresentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by LennartWisbar on 14.06.23.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeediOS/FeedPresentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by LennartWisbar on 14.06.23.
+//
+
+import Foundation
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
