11479082b64222bf6f8fb65cc068f4d7b46139a8
Lennart Wisbar <lewisbar.git@gmail.com>
2023-07-06 16:58:01 +0200

    Move `FeedUIComposer` helpers to separate files

---
+++ EssentialFeediOS/Feed UI/Composers/FeedViewAdapter.swift
@@ -0,0 +1,31 @@
+//
+//  FeedViewAdapter.swift
+//  EssentialFeediOS
+//
+//  Created by LennartWisbar on 06.07.23.
+//
+
+import UIKit
+
+final class FeedViewAdapter: FeedView {
+    private weak var controller: FeedViewController?
+    private var imageLoader: FeedImageDataLoader
+
+    init(controller: FeedViewController? = nil, imageLoader: FeedImageDataLoader) {
+        self.controller = controller
+        self.imageLoader = imageLoader
+    }
+
+    func display(_ viewModel: FeedViewModel) {
+        controller?.tableModel = viewModel.feed.map { model in
+            let adapter = FeedImageDataLoaderPresentationAdapter<WeakRefVirtualProxy<FeedImageCellController>, UIImage>(model: model, imageLoader: imageLoader)
+            let view = FeedImageCellController(delegate: adapter)
+
+            adapter.presenter = FeedImagePresenter(
+                view: WeakRefVirtualProxy(view),
+                imageTransformer: UIImage.init)
+
+            return view
+        }
+    }
+}
