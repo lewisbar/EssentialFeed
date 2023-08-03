fa4b39c0e8cbedf3925128a38efe4de7c0229c68
Lennart Wisbar <lewisbar.git@gmail.com>
2023-08-03 21:38:41 +0200

    Move `FeedImageViewModel` to separate file

--- EssentialFeed/Feed Presentation/FeedImagePresenter.swift
+++ EssentialFeed/Feed Presentation/FeedImagePresenter.swift
@@ -10,26 +10,6 @@ public protocol FeedImageView {
     func display(_ model: FeedImageViewModel<Image>)
 }
 
-public struct FeedImageViewModel<Image: Equatable>: Equatable {
-    public let description: String?
-    public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
-
-    public var hasLocation: Bool {
-        return location != nil
-    }
-
-    public init(description: String?, location: String?, image: Image?, isLoading: Bool, shouldRetry: Bool) {
-        self.description = description
-        self.location = location
-        self.image = image
-        self.isLoading = isLoading
-        self.shouldRetry = shouldRetry
-    }
-}
-
 public final class FeedImagePresenter<View: FeedImageView, Image> where View.Image == Image {
     let view: View
     private let imageTransformer: (Data) -> Image?
