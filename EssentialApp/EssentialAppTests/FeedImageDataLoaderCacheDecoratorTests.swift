4fa6e7928784e51d91d539c9a5ffda4ea9f2da3a
Lennart Wisbar <lewisbar.git@gmail.com>
2023-11-03 11:21:23 +0100

    `FeedImageDataLoaderCacheDecorator.loadImageData` delivers image data on loader success

---
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -0,0 +1,44 @@
+//
+//  FeedImageDataLoaderCacheDecoratorTests.swift
+//  EssentialAppTests
+//
+//  Created by LennartWisbar on 02.11.23.
+//
+
+import XCTest
+import EssentialFeed
+
+final class FeedImageDataLoaderCacheDecorator: FeedImageDataLoader {
+    private let decoratee: FeedImageDataLoaderSpy
+
+    init(decoratee: FeedImageDataLoaderSpy) {
+        self.decoratee = decoratee
+    }
+
+    func loadImageData(from url: URL, completion: @escaping (FeedImageDataLoader.Result) -> Void) -> FeedImageDataLoaderTask {
+        decoratee.loadImageData(from: url, completion: completion)
+    }
+}
+
+final class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
+    func test_load_deliversImageDataOnLoaderSuccess() {
+        let data = anyData()
+        let loader = FeedImageDataLoaderSpy()
+        let sut = FeedImageDataLoaderCacheDecorator(decoratee: loader)
+
+        let exp = expectation(description: "Wait for loading to complete")
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            switch receivedResult {
+            case let .success(receivedData):
+                XCTAssertEqual(receivedData, data)
+            case .failure:
+                XCTFail("Expected success, got \(receivedResult) instead")
+            }
+            exp.fulfill()
+        }
+
+        loader.complete(with: .success(data))
+        
+        wait(for: [exp], timeout: 1)
+    }
+}
