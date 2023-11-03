9a645593fe5fdabe2b82452fa69fd81437d208df
Lennart Wisbar <lewisbar.git@gmail.com>
2023-11-03 11:28:57 +0100

    `FeedImageDataLoaderCacheDecorator.loadImageData` delivers error on loader failure

--- EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
+++ EssentialApp/EssentialAppTests/FeedImageDataLoaderCacheDecoratorTests.swift
@@ -41,4 +41,25 @@ final class FeedImageDataLoaderCacheDecoratorTests: XCTestCase {
         
         wait(for: [exp], timeout: 1)
     }
+
+    func test_load_deliversErrorOnLoaderFailure() {
+        let error = anyNSError()
+        let loader = FeedImageDataLoaderSpy()
+        let sut = FeedImageDataLoaderCacheDecorator(decoratee: loader)
+
+        let exp = expectation(description: "Wait for loading to complete")
+        _ = sut.loadImageData(from: anyURL()) { receivedResult in
+            switch receivedResult {
+            case let .failure(receivedError):
+                XCTAssertEqual(receivedError as NSError, error)
+            case .success:
+                XCTFail("Expected failure, got \(receivedResult) instead")
+            }
+            exp.fulfill()
+        }
+
+        loader.complete(with: .failure(error))
+
+        wait(for: [exp], timeout: 1)
+    }
 }
