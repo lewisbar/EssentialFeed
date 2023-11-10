372ee6d6a00b4dad268c1e1ab40441785105dc06
Lennart Wisbar <lewisbar.git@gmail.com>
2023-11-10 16:28:44 +0100

    Fix potential but when cancelling requests in UITableView didEndDisplayingCell method. - This method is invoked after calling `reloadData`, so we'd be cancelling requests in the wrong models or crash in case the model has less items than the previous model
    Note: I could not reproduce the crash. I saw no test failure. I tried a few different things, but I'm moving on for now.

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -14,7 +14,8 @@ public protocol FeedViewControllerDelegate {
 
 public final class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching, FeedLoadingView, FeedErrorView {
     @IBOutlet private(set) public var errorView: ErrorView?
-    public var delegate: FeedViewControllerDelegate?
+
+    private var loadingControllers = [IndexPath: FeedImageCellController]()
 
     private var tableModel = [FeedImageCellController]() {
         didSet {
@@ -23,6 +24,8 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     private var onViewIsAppearing: ((FeedViewController) -> Void)?
+
+    public var delegate: FeedViewControllerDelegate?
 
     public override func viewDidLoad() {
         super.viewDidLoad()
@@ -50,6 +53,7 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     public func display(_ cellControllers: [FeedImageCellController]) {
+        loadingControllers = [:]
         tableModel = cellControllers
     }
 
@@ -90,10 +94,13 @@ public final class FeedViewController: UITableViewController, UITableViewDataSou
     }
 
     private func cellController(forRowAt indexPath: IndexPath) -> FeedImageCellController {
-        tableModel[indexPath.row]
+        let controller = tableModel[indexPath.row]
+        loadingControllers[indexPath] = controller
+        return controller
     }
 
     private func cancelCellControllerLoad(forRowAt indexPath: IndexPath) {
-        cellController(forRowAt: indexPath).cancelLoad()
+        loadingControllers[indexPath]?.cancelLoad()
+        loadingControllers[indexPath] = nil
     }
 }
