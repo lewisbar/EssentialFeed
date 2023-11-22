becd6af445d8dc1b6ef0777d1511b8f27b64147f
Lennart Wisbar <lewisbar.git@gmail.com>
2023-11-22 22:21:12 +0100

    Replace "FEED_VIEW_CONNECTION_ERROR" with "GENERIC_CONNECTION_ERROR"

--- EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
+++ EssentialApp/EssentialAppTests/FeedUIIntegrationTests.swift
@@ -120,7 +120,7 @@ final class FeedUIIntegrationTests: XCTestCase {
         XCTAssertEqual(sut.errorMessage, nil)
 
         loader.completeFeedLoadingWithError(at: 0)
-        XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+        XCTAssertEqual(sut.errorMessage, localized("GENERIC_CONNECTION_ERROR"))
 
         sut.simulateUserInitiatedFeedReload()
         XCTAssertEqual(sut.errorMessage, nil)
@@ -140,7 +140,7 @@ final class FeedUIIntegrationTests: XCTestCase {
 //        XCTAssertEqual(sut.errorMessage, nil)
 //
 //        loader.completeFeedLoadingWithError(at: 0)
-//        XCTAssertEqual(sut.errorMessage, localized("FEED_VIEW_CONNECTION_ERROR"))
+//        XCTAssertEqual(sut.errorMessage, localized("GENERIC_CONNECTION_ERROR"))
 //
 //        sut.simulateTapOnErrorMessage()
 //        XCTAssertEqual(sut.errorMessage, nil)
