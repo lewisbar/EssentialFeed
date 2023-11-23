1dc565eb68846037a40e29f388ec9b9f97ec16ac
Lennart Wisbar <lewisbar.git@gmail.com>
2023-11-23 22:50:00 +0100

    Rename and move ResourceErrorView and ViewModel to Shared module

---
+++ EssentialFeed/EssentialFeed/Shared Presentation/ResourceErrorView.swift
@@ -0,0 +1,12 @@
+//
+//  ResourceErrorView.swift
+//  EssentialFeed
+//
+//  Created by LennartWisbar on 23.11.23.
+//
+
+import Foundation
+
+public protocol ResourceErrorView {
+    func display(_ viewModel: ResourceErrorViewModel)
+}
