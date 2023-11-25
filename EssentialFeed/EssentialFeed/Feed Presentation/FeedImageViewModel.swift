a62d6991b2838f2e76c666c60337d4b85d2cea20
Lennart Wisbar <lewisbar.git@gmail.com>
2023-11-26 00:41:38 +0100

    Remove unused `FeedImagePresenter` logic

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedImageViewModel.swift
@@ -5,22 +5,16 @@
 //  Created by LennartWisbar on 03.08.23.
 //
 
-public struct FeedImageViewModel<Image> {
+public struct FeedImageViewModel {
     public let description: String?
     public let location: String?
-    public let image: Image?
-    public let isLoading: Bool
-    public let shouldRetry: Bool
 
     public var hasLocation: Bool {
         return location != nil
     }
 
-    public init(description: String?, location: String?, image: Image?, isLoading: Bool, shouldRetry: Bool) {
+    public init(description: String?, location: String?) {
         self.description = description
         self.location = location
-        self.image = image
-        self.isLoading = isLoading
-        self.shouldRetry = shouldRetry
     }
 }
--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImageViewModelTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedImageViewModelTests.swift
@@ -10,23 +10,17 @@ import EssentialFeed
 
 final class FeedImageViewModelTests: XCTestCase {
     func test_hasLocation_returnsFalseIfLocationIsNil() {
-        let sut = FeedImageViewModel<TestImage>(
+        let sut = FeedImageViewModel(
             description: "a description",
-            location: nil,
-            image: nil,
-            isLoading: false,
-            shouldRetry: false
+            location: nil
         )
         XCTAssertEqual(sut.hasLocation, false)
     }
 
     func test_hasLocation_returnsTrueIfLocationIsNotNil() {
-        let sut = FeedImageViewModel<TestImage>(
+        let sut = FeedImageViewModel(
             description: "a description",
-            location: "a location",
-            image: nil,
-            isLoading: false,
-            shouldRetry: false
+            location: "a location"
         )
         XCTAssertEqual(sut.hasLocation, true)
     }
