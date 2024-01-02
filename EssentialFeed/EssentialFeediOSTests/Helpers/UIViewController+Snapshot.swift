25fc940ef7789c9d8ef0173bea4eac380607216b
Lennart Wisbar <lewisbar.git@gmail.com>
2024-01-02 10:11:14 +0100

    Enable content size category config when taking a snapshot

--- EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
+++ EssentialFeed/EssentialFeediOSTests/Helpers/UIViewController+Snapshot.swift
@@ -19,7 +19,7 @@ struct SnapshotConfiguration {
     let layoutMargins: UIEdgeInsets
     let traitCollection: UITraitCollection
 
-    static func iPhone15Pro(style: UIUserInterfaceStyle) -> SnapshotConfiguration {
+    static func iPhone15Pro(style: UIUserInterfaceStyle, contentSize: UIContentSizeCategory = .medium) -> SnapshotConfiguration {
         return SnapshotConfiguration(
             size: CGSize(width: 393, height: 852),
             safeAreaInsets: UIEdgeInsets(top: 59, left: 0, bottom: 34, right: 0),
@@ -27,7 +27,7 @@ struct SnapshotConfiguration {
             traitCollection: UITraitCollection { traits in
                 traits.forceTouchCapability = .available
                 traits.layoutDirection = .leftToRight
-                traits.preferredContentSizeCategory = .medium
+                traits.preferredContentSizeCategory = contentSize
                 traits.userInterfaceIdiom = .phone
                 traits.horizontalSizeClass = .compact
                 traits.verticalSizeClass = .regular
