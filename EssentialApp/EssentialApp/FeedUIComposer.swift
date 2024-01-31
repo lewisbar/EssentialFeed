fa051e5d65c1cde3919608daef5148b45a8be07c
Lennart Wisbar <lewisbar.git@gmail.com>
2024-01-31 17:42:25 +0100

    Make feed loader closure return Paginated<FeedImage>

--- EssentialApp/EssentialApp/FeedUIComposer.swift
+++ EssentialApp/EssentialApp/FeedUIComposer.swift
@@ -13,10 +13,10 @@ import EssentialFeediOS
 final class FeedUIComposer {
     private init() {}
 
-    private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<[FeedImage], FeedViewAdapter>
+    private typealias FeedPresentationAdapter = LoadResourcePresentationAdapter<Paginated<FeedImage>, FeedViewAdapter>
 
     static func feedComposedWith(
-        feedLoader: @escaping () -> AnyPublisher<[FeedImage], Error>,
+        feedLoader: @escaping () -> AnyPublisher<Paginated<FeedImage>, Error>,
         imageLoader: @escaping (URL) -> FeedImageDataLoader.Publisher,
         selection: @escaping (FeedImage) -> Void = { _ in }
     ) -> ListViewController {
@@ -33,7 +33,7 @@ final class FeedUIComposer {
             ),
             loadingView: WeakRefVirtualProxy(feedController),
             errorView: WeakRefVirtualProxy(feedController), 
-            mapper: FeedPresenter.map
+            mapper: { $0 }
         )
 
         return feedController
