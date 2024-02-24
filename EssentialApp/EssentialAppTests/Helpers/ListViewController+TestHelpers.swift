d0f8541ed4cff8bda56d3db3c6021cd71b28d5c8
Lennart Wisbar <lewisbar.git@gmail.com>
2024-02-24 23:02:12 +0100

    Load more items until last page

--- EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
+++ EssentialApp/EssentialAppTests/Helpers/ListViewController+TestHelpers.swift
@@ -178,6 +178,10 @@ extension ListViewController {
         loadMoreFeedCell()?.message
     }
 
+    var canLoadMoreFeed: Bool {
+        loadMoreFeedCell() != nil
+    }
+
     private func loadMoreFeedCell() -> LoadMoreCell? {
         cell(row: 0, section: feedLoadMoreSection) as? LoadMoreCell
     }
