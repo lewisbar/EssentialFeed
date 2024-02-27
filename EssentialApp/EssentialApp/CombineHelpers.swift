2523225db7cccbb9d0e2c9eb224cbc7256e1b714
Lennart Wisbar <lewisbar.git@gmail.com>
2024-02-27 22:39:24 +0100

    Make FeedImageDataCache synchronous

--- EssentialApp/EssentialApp/CombineHelpers.swift
+++ EssentialApp/EssentialApp/CombineHelpers.swift
@@ -78,7 +78,7 @@ public extension Publisher where Output == Data {
 
 private extension FeedImageDataCache {
     func saveIgnoringResult(_ data: Data, for url: URL) {
-        save(data, for: url) { _ in }
+        try? save(data, for: url)
     }
 }
 
